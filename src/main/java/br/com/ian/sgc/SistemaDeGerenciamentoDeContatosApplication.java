3e666592913d44f18d3a0a70584e48426e002a8c
Ian <ianmichael.xp@gmail.com>
2021-04-30 21:13:08 -0300

    versionamento do projeto no padrão gitflow

---
+++ src/main/java/br/com/ian/sgc/SistemaDeGerenciamentoDeContatosApplication.java
@@ -0,0 +1,13 @@
+package br.com.ian.sgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGerenciamentoDeContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGerenciamentoDeContatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ian/sgc/SistemaDeGerenciamentoDeContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ian.sgc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGerenciamentoDeContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
