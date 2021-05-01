463ae7a14644b6bfd2f3cd4dd1ed06c3f6c37d79
Ian <ianmichael.xp@gmail.com>
2021-04-30 22:08:17 -0300

    Models Contato e Telefone

---
+++ src/main/java/br/com/ian/sgc/model/Contato.java
@@ -0,0 +1,53 @@
+package br.com.ian.sgc.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Contato {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String primeiroNome;
+	private String ultimoNome;
+	private String email;
+	List<Telefone> telefones = new ArrayList<Telefone>();
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getPrimeiroNome() {
+		return primeiroNome;
+	}
+	public void setPrimeiroNome(String primeiroNome) {
+		this.primeiroNome = primeiroNome;
+	}
+	public String getUltimoNome() {
+		return ultimoNome;
+	}
+	public void setUltimoNome(String ultimoNome) {
+		this.ultimoNome = ultimoNome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public List<Telefone> getTelefones() {
+		return telefones;
+	}
+	public void setTelefones(List<Telefone> telefones) {
+		this.telefones = telefones;
+	}
+}
