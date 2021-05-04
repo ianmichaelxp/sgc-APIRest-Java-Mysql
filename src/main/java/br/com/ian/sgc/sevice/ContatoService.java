eb261137e9dfddc8ab51e3eadb1dd337d45f593a
Ian <ianmichael.xp@gmail.com>
2021-05-03 21:32:03 -0300

    Implementado paginacao, ordenacao e filtragem

--- src/main/java/br/com/ian/sgc/sevice/ContatoService.java
+++ src/main/java/br/com/ian/sgc/sevice/ContatoService.java
@@ -1,36 +1,48 @@
 package br.com.ian.sgc.sevice;
 
-import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import br.com.ian.sgc.controller.dto.ContatoDTO;
 import br.com.ian.sgc.model.Contato;
 import br.com.ian.sgc.repository.ContatoRepository;
 import br.com.ian.sgc.repository.TelefoneRepository;
+import br.com.ian.sgc.sevice.exception.RegraNegocioException;
 
 @Service
 public class ContatoService {
 
+	
 	@Autowired
 	private ContatoRepository contatoRepository;
 	
 	@Autowired
 	private TelefoneRepository telefoneRepository;
 	
-	public List<ContatoDTO> listarContatos() {		
-		List<Contato> contatos = contatoRepository.findAll();
-		return ContatoDTO.contatoToDTO(contatos);
+	public Page<ContatoDTO> listarContatos(String primeiroNome, String email, Pageable paginacao) {		
+		
+		if(primeiroNome != null || email != null) {			
+			Page<Contato> contatos = contatoRepository.findByPrimeiroNomeOrEmailContaining(primeiroNome, email, paginacao);
+
+			return ContatoDTO.contatoToDTO(contatos);
+		}
+		else {
+			Page<Contato> contatos = contatoRepository.findAll(paginacao);
+			return ContatoDTO.contatoToDTO(contatos);
+		}
+		
 	}
 
 	public Contato obterProcessoPorId(Long id) {
 		Optional<Contato> contato = contatoRepository.findById(id);
 		if(!contato.isPresent()) {
-			throw new RuntimeException("Não foi possível localizar, contato não encontrado.");
+			throw new RegraNegocioException("Não foi possível localizar, contato não encontrado.");
 		}		
 		return contato.get();
 	}
